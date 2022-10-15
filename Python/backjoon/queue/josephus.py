04de612e2529d547edf319e075b86e340e2b405d
noteasymin <noteasymin@gmail.com>
2022-10-15 23:45:10 +0900

    Update josephus.py

--- Python/backjoon/queue/josephus.py
+++ Python/backjoon/queue/josephus.py
@@ -1,21 +1,7 @@
 from collections import deque
-import sys
 
-def josephus():
-    N, K = map(int,sys.stdin.readline().split())
-    J = deque(i+1 for i in range(N))
-    new_J = deque()
-    cnt = -1 + K
-    while True:
-        new_J.append(J[cnt])
-        del J[cnt]
+N, K = map(int, input().split())
 
-        cnt += K
+people = [i+1 for i in range(N)]
 
-        if cnt > len(J):
-            
-
-
-
-
-josephus()
+print(people)
