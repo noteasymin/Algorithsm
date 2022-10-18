cb0e1213b7af64e2b38de7ebf16aadc8ca492646
noteasymin <noteasymin@gmail.com>
2022-10-18 17:06:54 +0900

    Create 2667_단지번호붙이기_DFS.py

---
+++ Python/backjoon/dfs/2667_단지번호붙이기_DFS.py
@@ -0,0 +1,41 @@
+import sys
+input = sys.stdin.readline
+
+N = int(input())
+graph = []
+num = []
+for i in range(N):
+    graph.append(list(map(int, input().rstrip())))
+
+dx = [0, 0, 1, -1]
+dy = [1, -1, 0, 0]
+
+count = 0
+result = 0
+
+def DFS(x,y):
+    if x < 0 or x >= N or y < 0 or y >= N:
+        return False
+
+    if graph[x][y] == 1:
+        global count
+        count += 1
+        graph[x][y] = 0
+        for i in range(4):
+            nx = x + dx[i]
+            ny = y + dy[i]
+            DFS(nx, ny)
+        return True
+    return False
+
+for i in range(N):
+    for j in range(N):
+        if DFS(i,j) == True:
+            num.append(count)
+            result += 1
+            count = 0
+
+num.sort()
+print(result)
+for i in range(len(num)):
+    print(num[i])
