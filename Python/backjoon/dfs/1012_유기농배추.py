5ad5679877a4dbcc41cf3739152bd3e45f577506
noteasymin <noteasymin@gmail.com>
2022-10-18 23:21:06 +0900

    Update 1012_유기농배추.py

--- Python/backjoon/dfs/1012_유기농배추.py
+++ Python/backjoon/dfs/1012_유기농배추.py
@@ -1,5 +1,8 @@
+import sys
+sys.setrecursionlimit(10**6)
 
 def dfs(x, y):
+
     dx = [1, -1, 0, 0]
     dy = [0, 0, 1, -1]
 
@@ -7,23 +10,26 @@ def dfs(x, y):
         nx = x + dx[i]
         ny = y + dy[i]
 
-        if (0 <= nx <N) and (0 <= ny < M):
+        if (0 <= nx < N) and (0 <= ny < M):
             if graph[nx][ny] == 1:
                 graph[nx][ny] = -1
                 dfs(nx, ny)
+
 T = int(input())
-for _ in range(T):
+
+for i in range(T):
+    count = 0
     M, N, K = map(int, input().split())
-    cnt = 0
     graph = [[0 for i in range(M)] for i in range(N)]
 
     for i in range(K):
-        a, b = map(int,input().split())
+        a, b = map(int, input().split())
         graph[b][a] = 1
 
     for i in range(N):
         for j in range(M):
-            if graph[i][j] > 0:
+            if graph[i][j] == 1:
                 dfs(i,j)
-                cnt += 1
-    print(cnt)
+                count += 1
+
+    print(count)
