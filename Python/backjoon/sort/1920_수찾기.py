71c3e9a8e2bf9d5f4a8760c5bb084d951fabd3ce
noteasymin <noteasymin@gmail.com>
2022-10-22 22:57:46 +0900

    practice

--- Python/backjoon/sort/1920_수찾기.py
+++ Python/backjoon/sort/1920_수찾기.py
@@ -1,5 +1,23 @@
-N = int(input())
-num_dict = {}
+from sys import stdin, stdout
 
-for i in range(N):
-    num_dict[i] =
+n = stdin.readline()
+N = sorted(map(int, stdin.readline().split()))
+m = stdin.readline()
+M = map(int, stdin.readline().split())
+
+def binary(l, N, start, end):
+    if start > end:
+        return 0
+    m = (start + end) // 2
+
+    if l == N[m]:
+        return 1
+    elif l < N[m]:
+        return binary(l, N, start, m-1)
+    else:
+        return binary(l, N, m+1, end)
+
+for l in M:
+    start = 0
+    end = len(N) - 1
+    print(binary(l, N, start, end))
