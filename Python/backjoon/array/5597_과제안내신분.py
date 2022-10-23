c5337838d6984e6b9268994adbf8260987470b12
noteasymin <noteasymin@gmail.com>
2022-10-23 23:09:14 +0900

    practice

---
+++ Python/backjoon/array/5597_과제안내신분.py
@@ -0,0 +1,8 @@
+lst = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30]
+
+for i in range(28):
+    v = int(input())
+    lst.remove(v)
+
+print(min(lst))
+print(max(lst))
