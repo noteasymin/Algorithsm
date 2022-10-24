073a9b65b301c0dfdeaa0daa48075d4bfd6176ac
noteasymin <noteasymin@gmail.com>
2022-10-25 01:36:54 +0900

    Update 14888_연산자 끼워넣기.py

--- Python/backjoon/backtracking.py/14888_연산자 끼워넣기.py
+++ Python/backjoon/backtracking.py/14888_연산자 끼워넣기.py
@@ -2,8 +2,8 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
-num_lst = list(map(int, input().split()))
-operator_lst = list(map(int, input().split()))
+num = list(map(int, input().split()))
+op = list(map(int, input().split()))
 
 
 maximum = -1e9
@@ -12,3 +12,18 @@ minimum = 1e9
 def dfs(depth, total, plus, minus, multiply, divide):
     global maximum, minimum
     if depth == N:
+        maximum = max(total, maximum)
+        minimum = min(total, minimum)
+
+    if plus:
+        dfs(depth + 1, total + num[depth], plus - 1, minus, multiply, divide)
+    if minus:
+        dfs(depth + 1, total - num[depth], plus , minus-1, multiply, divide)
+    if multiply:
+        dfs(depth + 1, total * num[depth], plus, minus, multiply-1, divide)
+    if divide:
+        dfs(depth + 1, int(total / num[depth]), plus, minus, multiply, divide-1)
+
+dfs(1, num[0], op[0], op[1], op[2], op[3])
+print(maximum)
+print(minimum)
