375d6fda15281788fa5c58f9dab0f6f509db1796
noteasymin <noteasymin@gmail.com>
2022-10-25 23:36:43 +0900

    Create 2566_최댓값.py

---
+++ Python/backjoon/2차원배열/2566_최댓값.py
@@ -0,0 +1,12 @@
+arr = []
+max_num = -1
+
+for i in range(9):
+    temp = list(map(int, input().split()))
+    if max_num < max(temp):
+        max_num = max(temp)
+        loc = i
+    arr.append(temp)
+
+print(max_num)
+print(loc + 1, arr[loc].index(max_num) + 1)2
