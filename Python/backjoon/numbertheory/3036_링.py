b830aae0a74b6b06420c54ec7b47c03215601c5b
noteasymin <noteasymin@gmail.com>
2022-10-27 00:08:09 +0900

    Update 3036_링.py

--- Python/backjoon/numbertheory/3036_링.py
+++ Python/backjoon/numbertheory/3036_링.py
@@ -1,3 +1,4 @@
+import math
 N = int(input())
 
 lst = list(map(int, input().split()))
@@ -8,3 +9,8 @@ for i in lst:
     if num % i == 0:
         answer = num // i
         print(f'{answer}/1')
+    else:
+        answer = math.lcm(num, i)
+        ans1 = answer // i
+        ans2 = answer // num
+        print(f'{ans1}/{ans2}')
