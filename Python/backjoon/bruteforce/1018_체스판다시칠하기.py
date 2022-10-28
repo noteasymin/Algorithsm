b485fb62841ac71dd5e82e8307759a3a3d0f16de
noteasymin <noteasymin@gmail.com>
2022-10-28 20:43:56 +0900

    practice

--- Python/backjoon/bruteforce/1018_체스판다시칠하기.py
+++ Python/backjoon/bruteforce/1018_체스판다시칠하기.py
@@ -1,13 +1,28 @@
-n, m = map(int, input().split())
-board = []
-result = []
 
-for _ in range(n):
+N, M = map(int, input().split())
+board = []
+
+for i in range(N):
     board.append(input())
 
-for i in range(n-7):
-    for j in range(m-7):
-        draw1 = 0
-        draw2 = 0
-        for a in range(i, i+8):
-            for b in range(j, j+8:)
+repair = []
+
+for i in range(N-7):
+    for j in range(M-7):
+        first_W = 0
+        first_B = 0
+        for k in range(i, i+8):
+            for l in range(j, j+8):
+                if (k+l) % 2 == 0:
+                    if board[k][l] != 'W':
+                        first_W = first_W+1
+                    if board[k][l] != 'B':
+                        first_B = first_B + 1
+                else:
+                    if board[k][l] != 'B':
+                        first_W = first_W + 1
+                    if board[k][l] != 'W':
+                        first_B = first_B + 1
+        repair.append(first_W)
+        repair.append(first_B)
+print(min(repair))
