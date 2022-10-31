1d41faf2c868424371234cdd8376ecf8698c65be
noteasymin <noteasymin@gmail.com>
2022-10-31 16:33:01 +0900

    Create 2805_나무자르기.py

---
+++ Python/backjoon/구현/2805_나무자르기.py
@@ -0,0 +1,20 @@
+N, M = map(int, input().split())
+tree = list(map(int, input().split()))
+
+start, end = 1, max(tree)
+
+while start <= end:
+    mid = (start + end) // 2
+
+    log = 0 # 벌목된 나무의 총합
+
+    for i in tree:
+        if i >= mid:
+            log += i - mid
+
+
+    if log >= M:
+        start = mid + 1
+    else:
+        end = mid - 1
+print(end)
