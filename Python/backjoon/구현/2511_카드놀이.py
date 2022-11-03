06e652f617078177138258c1fa152ad5e6b0e149
noteasymin <noteasymin@gmail.com>
2022-11-04 00:07:29 +0900

    Update 2511_카드놀이.py

--- Python/backjoon/구현/2511_카드놀이.py
+++ Python/backjoon/구현/2511_카드놀이.py
@@ -1,14 +1,18 @@
 lst1 = list(map(int, input().split()))
 lst2 = list(map(int, input().split()))
 answer = [0, 0]
+who = []
 for i in range(10):
     if lst1[i] > lst2[i]:
         answer[0] += 3
+        who.append('A')
     elif lst2[i] > lst1[i]:
         answer[1] += 3
+        who.append('B')
     else:
         answer[0] += 1
         answer[1] += 1
+        who.append('D')
 
 print(answer[0],answer[1])
 
@@ -19,7 +23,12 @@ elif answer[0] > answer[1]:
 elif answer[0] < answer[1]:
     print('B')
 elif answer[0] == answer[1]:
-    if lst1[-1] > lst2[-1]:
-        print('A')
-    else:
-        print('B')
+    for i in reversed(who):
+        if i == 'A':
+            print('A')
+            break
+        elif i == 'B':
+            print('B')
+            break
+        else:
+            continue
