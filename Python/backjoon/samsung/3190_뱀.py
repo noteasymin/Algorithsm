1d4d0a3ed09b7911b397bd3c9e2d88f77672885f
noteasymin <noteasymin@gmail.com>
2022-11-11 21:31:53 +0900

    Update 3190_뱀.py

--- Python/backjoon/samsung/3190_뱀.py
+++ Python/backjoon/samsung/3190_뱀.py
@@ -7,8 +7,11 @@ graph = [[0 for _ in range(N)] for i in range(N)]
 for i in range(K):
     a,b = map(int, input().split())
     graph[a-1][b-1] = 1
+snake = deque([[0,0]])
 
 graph[0][0] = 2
+head = [0,0]
+tail = [0,0]
 direction = ['E','S','W','N']
 loc = 0
 L = int(input())
@@ -18,14 +21,36 @@ for i in range(L):
     command = input().split()
     command_lst.append(command)
 
-print(command_lst)
 dx = [0,1,0,-1]
 dy = [1,0,-1,0]
 time = 0
 
 while True:
     time += 1
-    if int(command_lst[0][0]) == time and len(command_lst) > 0:
+
+
+    nx = head[0] + dx[loc]
+    ny = head[1] + dy[loc]
+
+    if 0 <= nx < N and 0 <= ny < N and graph[nx][ny] != 2:
+        if graph[nx][ny] == 0:
+            head[0] = nx
+            head[1] = ny
+            snake.append([nx,ny])
+            tail[0] = snake[0][0]
+            tail[1] = snake[0][1]
+            snake.popleft()
+            graph[nx][ny] = 2
+            graph[tail[0]][tail[1]] = 0
+        elif graph[nx][ny] == 1:
+            snake.append([nx, ny])
+            graph[nx][ny] = 2
+            head[0] = nx
+            head[1] = ny
+    elif nx < 0 or nx >= N or ny < 0 or ny >= N or graph[nx][ny] == 2:
+        print(time)
+        break
+    if len(command_lst) > 0 and int(command_lst[0][0]) == time:
         if command_lst[0][1] == 'D':
             loc += 1
             loc %= 4
@@ -33,4 +58,3 @@ while True:
             loc -= 1
             loc %= 4
         command_lst.popleft()
-    print(loc)
