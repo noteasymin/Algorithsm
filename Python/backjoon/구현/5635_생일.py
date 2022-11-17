963525f015412890283e2dc53f58fdcc8d99f9f2
noteasymin <noteasymin@gmail.com>
2022-11-17 16:22:51 +0900

    change

--- Python/backjoon/구현/5635_생일.py
+++ Python/backjoon/구현/5635_생일.py
@@ -1,6 +1,25 @@
+from datetime import datetime
+import time
+
 n = int(input())
-max_age = 2011
-min_age = 1989
-answer = []
+older = 0
+younger = 99999999999999999
+answer = ['','']
 for i in range(n):
-    a = input().split()
+    lst = input().split()
+    date = []
+    date.append(lst[3])
+    date.append(lst[2])
+    date.append(lst[1])
+    date = '-'.join(date)
+    tstmp = time.mktime(datetime.strptime(date, '%Y-%m-%d').timetuple())
+    if int(tstmp) > older:
+        older = tstmp
+        answer[0] = lst[0]
+
+    if int(tstmp) < younger:
+        younger = tstmp
+        answer[1] = lst[0]
+
+print(answer[0])
+print(answer[1])
