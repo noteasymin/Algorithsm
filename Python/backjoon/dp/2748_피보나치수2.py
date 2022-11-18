ead42201529d63e10b76d981b0343390f20ff23f
noteasymin <noteasymin@gmail.com>
2022-11-18 22:03:51 +0900

    practice

---
+++ Python/backjoon/dp/2748_피보나치수2.py
@@ -0,0 +1,15 @@
+lst = [0, 1]
+cnt = 2
+
+for i in range(90):
+    lst.append(lst[cnt-2] + lst[cnt-1])
+    cnt += 1
+
+N = int(input())
+
+if N == 0:
+    print(0)
+elif N == 1:
+    print(1)
+else:
+    print(lst[N])
