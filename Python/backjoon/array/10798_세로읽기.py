8f8f08e61267dee0193399e6b942b372bcf7f710
JM <noteasymin@gmail.com>
2023-03-29 02:59:15 +0900

    Update 10798_세로읽기.py

--- Python/backjoon/array/10798_세로읽기.py
+++ Python/backjoon/array/10798_세로읽기.py
@@ -3,15 +3,16 @@ from collections import deque
 
 def solution():
     lst = deque()
+    new_lst = deque([[] for i in range(15)])
     length = 0
 
     for _ in range(5):
         s = input()
-        length += len(s)
-        lst.append(s)
-    print(length)
+        for i in range(len(s)):
+            new_lst[i].append(s[i])
 
-
+    for i in range(15):
+        print("".join(new_lst[i]), end="")
 
 
 solution()
