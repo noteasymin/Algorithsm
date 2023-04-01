80a6dc1aab3f93794ceb85f20169b04076bac350
JM <noteasymin@gmail.com>
2023-04-01 23:50:34 +0900

    Update 10812_바구니 순서 바꾸기.py

--- Python/backjoon/array/10812_바구니 순서 바꾸기.py
+++ Python/backjoon/array/10812_바구니 순서 바꾸기.py
@@ -1,3 +1,20 @@
+from collections import deque
+
+
 def solution():
     n, m = map(int, input().split())
 
+    lst = deque([i for i in range(1, n + 1)])
+    print(lst)
+
+    for _ in range(m):
+        i, j, k = map(int, input().split())
+        i -= 1
+        j -= 1
+
+        for _ in range(k):
+            lst.insert()
+
+
+
+solution()
