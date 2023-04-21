cb6c316892bc0f39b1a4cbe7dc881957e9df63a4
JM <noteasymin@gmail.com>
2023-04-21 23:28:37 +0900

    Update 2720_세탁소 사장 동혁.py

--- Python/backjoon/basicmath1/2720_세탁소 사장 동혁.py
+++ Python/backjoon/basicmath1/2720_세탁소 사장 동혁.py
@@ -1,2 +1,30 @@
 def solution():
-    
+    t = int(input())
+
+    for i in range(t):
+        quarter = 0
+        dime = 0
+        nickel = 0
+        penny = 0
+        money = int(input())
+
+        if money >= 25:
+            quarter = money // 25
+            money -= quarter * 25
+
+        if money >= 10:
+            dime = money // 10
+            money -= dime * 10
+
+        if money >= 5:
+            nickel = money // 5
+            money -= nickel * 5
+
+        if money >= 1:
+            penny = money // 1
+            money -= penny * 1
+
+        print(quarter, dime, nickel, penny)
+
+
+solution()
