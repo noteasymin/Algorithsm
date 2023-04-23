0dc1d0bc179293bfb31e73bc32acad38c4b22e9d
JM <noteasymin@gmail.com>
2023-04-23 23:49:11 +0900

    Update 14215_세 막대.py

--- Python/backjoon/math/14215_세 막대.py
+++ Python/backjoon/math/14215_세 막대.py
@@ -1,4 +1,13 @@
 def solution():
-    a, b, c = map(int,input().split())
+    lines = list(map(int, input().split()))
+    lines.sort()
 
-      
+    cond = lines[0] + lines[1]
+
+    if cond <= lines[2]:
+        print(lines[0] + lines[1] + cond - 1)
+    else:
+        print(sum(lines))
+
+
+solution()
