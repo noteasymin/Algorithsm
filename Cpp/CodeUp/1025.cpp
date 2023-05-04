8193a4117a9a5a5e69a656acc0d6c635ca858a67
JM <noteasymin@gmail.com>
2023-05-05 02:11:46 +0900

    Create 1025.cpp

---
+++ Cpp/CodeUp/1025.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+
+    int a,b,c,d,e;
+    int number = 10000;
+    scanf("%1d%1d%1d%1d%1d", &a, &b, &c, &d, &e);
+
+    printf("[%d]\n", a*10000);
+    printf("[%d]\n", b*1000);
+    printf("[%d]\n", c*100);
+    printf("[%d]\n", d*10);
+    printf("[%d]\n", e);
+    
+    return 0;
+}
