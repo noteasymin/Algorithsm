019f6faba60a1567da9525d59a06f96e8550652d
JM <noteasymin@gmail.com>
2023-05-11 15:07:52 +0900

    Create 2485_가로수.py

---
+++ Python/backjoon/basicmath2/2485_가로수.py
@@ -0,0 +1,42 @@
+import sys
+
+
+def solution():
+    # 이미 심어져 있는 가로수 수
+    N = int(sys.stdin.readline())
+
+    # 첫 가로수 위치
+    a = int(sys.stdin.readline())
+
+    # 가로수들 사이의 값을 저장할 배열
+    arr = []
+
+    # 가로수들 사이의 간격 저장
+    for i in range(N - 1):
+        num = int(sys.stdin.readline())
+        arr.append(num - a)
+        a = num
+
+    # arr에 들어있는 모든 수들의 최대공약수 찾기
+    g = arr[0]
+    for j in range(1, len(arr)):
+        g = gcd(g, arr[j])
+
+    # 둘 사이에 심을 가로수 개수: 간격 // 최대공약수 - 1
+    result = 0
+    for each in arr:
+        result += each // g - 1
+    print(result)
+
+
+def gcd(a, b):
+    while b:
+        if a > b:
+            a, b = b, a
+
+        b %= a
+
+    return a
+
+
+solution()
