aa20e0016b5c1999c4a8979f77bf6443d7e189c2
JM <noteasymin@gmail.com>
2023-05-12 18:14:56 +0900

    Update 비밀 메뉴.py

--- Python/Softeer/Level2/비밀 메뉴.py
+++ Python/Softeer/Level2/비밀 메뉴.py
@@ -6,21 +6,15 @@ input = sys.stdin.readline
 
 def solution():
     m, n, k = map(int, input().split())
-    secret_menu = list(map(int, input().split()))
-    check = 0
+
     if m > n:
         print("normal")
         exit()
-    order = list(map(int, input().split()))
 
-    order = list(set(itertools.permutations(order, m)))
+    secret = "".join(list(map(str, input().split())))
+    menu = "".join(list(map(str, input().split())))
 
-    for i in order:
-        if secret_menu == list(i):
-            check = 1
-            break
-
-    if check == 1:
+    if secret in menu:
         print("secret")
     else:
         print("normal")
