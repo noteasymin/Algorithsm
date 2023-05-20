556e52130dc6ec1ea02ba62262b8660762ed3def
JM <noteasymin@gmail.com>
2023-05-20 23:19:13 +0900

    Update 2005_파스칼의 삼각형.py

--- Python/swea/d2/2005_파스칼의 삼각형.py
+++ Python/swea/d2/2005_파스칼의 삼각형.py
@@ -1,3 +1,22 @@
 def solution():
     T = int(input())
     for tc in range(1, T + 1):
+        n = int(input())
+
+        answer = []
+
+        for i in range(n):
+            temp = []
+            for j in range(i + 1):
+                if j == 0 or j == i:
+                    temp.append(1)
+                else:
+                    temp.append(answer[i - 1][j] + answer[i - 1][j - 1])
+
+            answer.append(temp)
+        print(f'#{tc}')
+        for i in answer:
+            print(*i)
+
+
+solution()
