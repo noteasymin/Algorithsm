315703379c21d0283ed0fd8b00eca0ff36ad43d9
JM <noteasymin@gmail.com>
2023-05-30 17:30:12 +0900

    practice

---
+++ Python/baekjoon/심화/25192_인사성 밝은 곰곰이.py
@@ -0,0 +1,25 @@
+import sys
+
+input = sys.stdin.readline
+
+
+def solution():
+    n = int(input())
+    result = set()
+    cnt = 0
+    
+    for i in range(n):
+        nickname = input().rstrip()
+
+        if nickname != 'ENTER':
+            if nickname not in result:
+                cnt += 1
+                result.add(nickname)
+
+        elif nickname == 'ENTER':
+            result.clear()
+
+    print(cnt)
+
+
+solution()
