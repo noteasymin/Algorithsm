66d44b03a70d5bc5adc93f2cd5cbf293e9028d62
JM <noteasymin@gmail.com>
2023-06-21 18:10:14 +0900

    Update 가장 큰 수.py

--- Python/programmers/Practice/Lv2/가장 큰 수.py
+++ Python/programmers/Practice/Lv2/가장 큰 수.py
@@ -2,11 +2,16 @@ def solution(numbers):
     numbers = list(map(str, numbers))
 
     # 비교 함수를 lambda 함수로 정의하여 사용합니다.
-    compare = lambda x: x*3
-
+    compare = lambda x: x * 3
+    print(compare)
     numbers.sort(key=lambda x: compare(x), reverse=True)
 
     if numbers[0] == "0":
         return "0"
 
+    print("".join(numbers))
     return "".join(numbers)
+
+
+numbers = [6, 10, 2]
+solution(numbers)
