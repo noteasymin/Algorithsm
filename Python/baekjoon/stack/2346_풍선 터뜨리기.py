5b66445946748d42b0c0e7f3fc2da1210fd33e0c
noteasymin <noteasymin@gmail.com>
2023-08-20 15:00:22 +0900

    Create 2346_풍선 터뜨리기.py

---
+++ Python/baekjoon/stack/2346_풍선 터뜨리기.py
@@ -0,0 +1,24 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+
+def solution():
+    n = int(input())
+    q = deque(enumerate(map(int, input().split())))
+    ans = []
+
+    while q:
+        idx, paper = q.popleft()
+        ans.append(idx + 1)
+
+        if paper > 0:
+            q.rotate(-(paper - 1))
+        elif paper < 0:
+            q.rotate(-paper)
+
+    print(' '.join(map(str, ans)))
+
+
+solution()
