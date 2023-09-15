eca228cc54ed9ff1cf1b1f0d14a66ce8ae463dbb
noteasymin <noteasymin@gmail.com>
2023-09-15 17:38:17 +0900

    Create Main.java

---
+++ Java/Goorm-Challenge/src/main/java/com/jimin/study/Main.java
@@ -0,0 +1,11 @@
+package com.jimin.study;
+
+import com.jimin.study.goorm.day1;
+import com.jimin.study.goorm.day2;
+import com.jimin.study.goorm.day3;
+
+public class Main {
+    public static void main(String[] args) {
+        day3.main();
+    }
+}
