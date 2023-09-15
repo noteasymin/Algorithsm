8ed8773eb4a49fdb253eb685209a9fdd24e61b8a
noteasymin <noteasymin@gmail.com>
2023-09-15 15:53:04 +0900

    create

---
+++ Java/Goorm-Challenge/day1_운동 중독 플레이어.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int W = sc.nextInt();
+        int R = sc.nextInt();
+        sc.next
+        System.out.println((int)(W * (1 + (double)R / 30)));
+
+    }
+}
