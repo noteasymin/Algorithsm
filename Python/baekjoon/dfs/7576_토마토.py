9cf4f1fa68bee00b985ac8cc86e9f71c187377f5
noteasymin <noteasymin@gmail.com>
2023-09-15 15:02:10 +0900

    Update 7576_토마토.py

--- Python/baekjoon/dfs/7576_토마토.py
+++ Python/baekjoon/dfs/7576_토마토.py
@@ -1,7 +1,35 @@
 from collections import deque
-N, M = map(int, input().split())
-dx = [0, 0, -1, 1]
-dy = [-1, 1, 0, 0]
-graph = []
-riped = []
 
+M, N = map(int, input().split())
+graph = [list(map(int, input().split())) for _ in range(N)]
+queue = deque([])
+dx, dy = [-1, 1, 0, 0], [0, 0, -1, 1]
+res = 0
+
+for i in range(N):
+    for j in range(M):
+        if graph[i][j] == 1:
+            queue.append([i, j])
+
+
+def bfs():
+    while queue:
+        x, y = queue.popleft()
+
+        for i in range(4):
+            nx, ny = dx[i] + x, dy[i] + y
+
+            if 0 <= nx < N and 0 <= ny < M and graph[nx][ny] == 0:
+                graph[nx][ny] = graph[x][y] + 1
+                queue.append([nx, ny])
+
+
+bfs()
+
+for i in graph:
+    for j in i:
+        if j == 0:
+            print(-1)
+            exit(0)
+    res = max(res, max(i))
+print(res - 1)
